4fcea277c5e1d9e651ecb575bc7dd8b76f140898
Leidy Laura Martinez Arias <leidylaurama@gmail.com>
2021-12-28 17:34:00 -0500

    changing element ids 'stsp' > 'start-stop' and 'setting' > 'settings'

---
+++ index.js
@@ -0,0 +1,5 @@
+const progressBar = document.querySelector('.outerRing'),
+  clockMin = document.querySelector('#minutes'),
+  clockSec = document.querySelector('#seconds'),
+  startStop = document.querySelector('#start-stop'),
+  settings = document.querySelector('#settings');
